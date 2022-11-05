0022eedf1387ecb034ddbe4a4f90ae48bf160d80
Alexander Alemayhu <alexander@alemayhu.com>
2022-11-05 13:53:28 +0100

    feat: add support for getting text content from zip files

--- helpers/zip/test_read_zip.py
+++ helpers/zip/test_read_zip.py
@@ -2,28 +2,31 @@ import unittest
 import os
 import zipfile
 
-from helpers.zip.zip import get_files_in_zip_file
+from helpers.zip.zip import get_files_in_zip_file, get_text_in_zip_file
 
 EXAMPLE_ZIP_PATH = "create_deck-example.zip"
-EXAMPLE_FILE_PATH = '/tmp/input.txt'
 
 
 class MyTestCase(unittest.TestCase):
-    def setUp(self):
-        with open(EXAMPLE_FILE_PATH, 'w') as f:
-            f.write('tull')
+    @classmethod
+    def setUpClass(cls):
         filepath = os.path.join(os.getcwd(), EXAMPLE_ZIP_PATH)
         with zipfile.ZipFile(filepath, 'a') as zipf:
-            zipf.write(EXAMPLE_ZIP_PATH, "input.txt")
+            zipf.writestr("input.txt", 'tull')
 
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(cls):
         os.remove(EXAMPLE_ZIP_PATH)
-        os.remove(EXAMPLE_FILE_PATH)
 
     def test_get_files(self):
         expect_files = ['input.txt']
         actual_files = get_files_in_zip_file(EXAMPLE_ZIP_PATH)
         self.assertEqual(expect_files, actual_files)
+
+    def test_get_text_content(self):
+        expected = "tull"
+        actual = get_text_in_zip_file(EXAMPLE_ZIP_PATH, "input.txt")
+        self.assertEqual(expected, actual)
 
 
 if __name__ == '__main__':
--- helpers/zip/zip.py
+++ helpers/zip/zip.py
@@ -1,6 +1,19 @@
 import zipfile
+import io
 
 
-def get_files_in_zip_file(file_path):
-    payload = zipfile.ZipFile(file_path, 'r')
+def get_file(zip_path):
+    return zipfile.ZipFile(zip_path, 'r')
+
+
+def get_files_in_zip_file(zip_path):
+    payload = get_file(zip_path)
     return payload.namelist()
+
+
+def get_text_in_zip_file(zip_path, file_name):
+    payload = get_file(zip_path)
+    file = payload.open(file_name, 'r')
+    contents = file.read().decode('utf-8')
+    file.close()
+    return contents
