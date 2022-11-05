aedadf60e9dcad4e8c724fd8e1711cfc85c4aece
Alexander Alemayhu <alexander@alemayhu.com>
2022-11-05 12:50:41 +0100

    feat: add version file

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from typing import Union
 from fastapi import FastAPI
 
+from version import version
+
 app = FastAPI()
 
 
 @app.get("/")
 def read_version():
-    return "0.0.0"
+    return version
