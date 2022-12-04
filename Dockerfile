eac437aeac570a47e8ce64edb02fcff3346b3d61
Alexander Alemayhu <alexander@alemayhu.com>
2022-12-04 17:08:14 +0100

    chore: prepare first deployment

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.9
+
+WORKDIR /api
+
+COPY ./requirements.txt /api/requirements.txt
+
+RUN pip install --no-cache-dir --upgrade -r /api/requirements.txt
+
+COPY . /api/
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-from typing import Union
 from fastapi import FastAPI
 
-from version import version
+from __version__ import version
 
-app = FastAPI()
+description = """
+The create_deck API helps you create Anki flashcards fast. 🌟
+"""
+
+tos = "https://alemayhu.notion.site/Terms-of-services-931865161517453b99fb6495e400061d"
+
+app = FastAPI(
+    title="create_deck API",
+    description=description,
+    version=version,
+    terms_of_service=tos,
+    contact={
+        "name": "Alexander Alemayhu",
+        "url": "https://alemayhu.com",
+        "email": "alexander@alemayhu.com"
+    },
+    license_info={
+        "name": "MIT LICENSE",
+        "url": "https://github.com/2anki/create_deck/blob/main/LICENSE"
+    }
+)
 
 
 @app.get("/")
 def read_version():
-    return version
+    return version.encode("utf-8")
