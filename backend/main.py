1b63a044ccac91b35d70361249c42f460ea4f766
Alexander Alemayhu <alexander@alemayhu.com>
2025-05-04 09:22:39 +0200

    feat: change default port
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- backend/main.py
+++ backend/main.py
@@ -24,4 +24,4 @@ async def status():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=31415)
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   apps: [{
-    name: "2anki-backend",
+    name: "napi",
     script: "./backend/main.py",
     interpreter: "python3",
     args: "--reload",
     env: {
       NODE_ENV: "development",
-      PORT: 8000,
+      PORT: 31415,
     },
     env_production: {
       NODE_ENV: "production",
-      PORT: 8000,
+      PORT: 31415,
     },
   }],
 };
